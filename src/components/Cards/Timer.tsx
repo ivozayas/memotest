ab27eb9c7813232fa164f577c69382412a85e12d
Ivo Zayas <ivozayas2014@gmail.com>
2025-01-07 16:55:11 -0300

    mmmm

--- src/components/Cards/Timer.tsx
+++ src/components/Cards/Timer.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import React, { useState, useEffect, useRef } from 'react';
+import React, { useState, useEffect, useRef, useMemo, useCallback } from 'react';
 import { cardStore } from '@/store/card';
 import TimerText from './TimeText';
 
@@ -12,6 +12,17 @@ export default function Timer(){
     const [time, setTime] = useState(0)
     const intervalRef = useRef<number | NodeJS.Timeout | null>(null)
   
+
+    const formattedTime = useMemo(() => {
+    const totalSeconds = Math.floor(time / 1000);
+    const minutes = Math.floor(totalSeconds / 60);
+    const seconds = totalSeconds % 60;
+    const centiseconds = Math.floor((time % 1000) / 10);
+
+    const pad = (num: number) => String(num).padStart(2, '0');
+    return `${pad(minutes)}:${pad(seconds)}:${pad(centiseconds)}`;
+  }, [time])
+    
     useEffect(() => {
         if (!won) {
             setTime(0)
@@ -30,6 +41,7 @@ export default function Timer(){
         }
     }, [won])
 
+    
     const formatTime = (milliseconds: number) => {
         const totalSeconds = Math.floor(milliseconds / 1000)
         const minutes = Math.floor(totalSeconds / 60)
